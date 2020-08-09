eaeb71bb15db42094d407fd750da578a65b3e94d
ertugrul <ertgrltkgz6027@gmail.com>
2020-08-09 21:24:41 +0200

    first commit

---
+++ src/git_project_turkce/ilkclass.java
@@ -0,0 +1,22 @@
+package git_project_turkce;
+
+public class ilkclass {
+	
+
+		public static void main(String[] args) {
+			 // yeni bir multidimensionalArray olusturun ve tum elemanlarinin toplamini yazdirin
+
+			System.out.println("hadi canim");
+			int mda2[][]= {{1,2},{3},{4,5,6}};
+			int sum=0;
+			for (int i = 0; i < mda2.length; i++) {
+				for (int j = 0; j < mda2[i].length; j++) {
+					sum=sum+mda2[i][j];
+				}
+			}	
+			
+		}}
+			
+			
+			
+			
